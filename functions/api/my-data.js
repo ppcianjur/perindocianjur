acfc0fade4931efddca250da4beb5147eb55740d
ppcianjur <pperindocianjur@gmail.com>
2026-02-11 17:37:35 +0700

    Update my-data.js

--- functions/api/my-data.js
+++ functions/api/my-data.js
@@ -2,10 +2,12 @@ export async function onRequestGet(context) {
     const { searchParams } = new URL(context.request.url);
     const username = searchParams.get('user');
     
+    // UPDATE QUERY: Join ke tabel kecamatan juga
     const { results } = await context.env.DB.prepare(`
-        SELECT p.*, d.nama_desa 
+        SELECT p.*, d.nama_desa, k.nama_kecamatan
         FROM pengurus p
         LEFT JOIN desa d ON p.kode_desa_lengkap = d.kode_desa_lengkap
+        LEFT JOIN kecamatan k ON d.kode_kec = k.kode_kec
         WHERE p.creator = ?
         ORDER BY p.created_at DESC
     `).bind(username).all();
